17f33edfd1ba6a9443b5d35118b5066dbfa1b3e9
Alexander Contreras <alex-c-r@hotmail.es>
2026-01-24 18:43:46 -0500

    ejercicio 3 secuencial
    area de pentagono regular

---
+++ 1 Secuencial/3_area_pentagono_reg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Determinar el área de un pentágono regular
+
+int main(){
+	double lado, apotema, area;
+	cout<<"Ingrese un lado"<<endl;
+	cin>>lado;
+	cout<<"Ingrese el apotema"<<endl;
+	cin>>apotema;
+	
+	area=(5*lado*apotema)/2
+	cout<<"El área del pentágono es: "<<area<<endl;
+	
+	return 0;
+}
